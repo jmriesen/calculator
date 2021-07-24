268174e22fb9b4fffffe450b2cfcb41b8d8c1a5c
jmriesen <jmriesen@iastate.edu>
2021-07-24 14:24:01 -0500

    Renamed tests using should.
    I saw a YouTube video witch said one tip for capturing Behavior is to use should
    in test names. So I thought I would try it.

--- src/main.rs
+++ src/main.rs
@@ -76,7 +76,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn read_variable_value(){
+    fn should_read_variable_value(){
         let mut variables = VariableMap::new();
         variables.insert(String::from("a"),7);
         assert_eq!(Postfix::new("a").evaluate(&variables), Ok(7));
@@ -86,7 +86,7 @@ mod tests {
     }
 
     #[test]
-    fn extra_spaces(){
+    fn should_ignore_extra_white_space(){
         assert_eq!(Postfix::new("8 ").evaluate(&Default::default()), Ok(8));
         assert_eq!(Postfix::new(" 8").evaluate(&Default::default()), Ok(8));
         assert_eq!(Postfix::new("    8").evaluate(&Default::default()), Ok(8));
@@ -96,7 +96,7 @@ mod tests {
     }
 
     #[test]
-    fn operate_on_variables(){
+    fn should_be_able_to_use_variables_in_expretions(){
         let mut variables = VariableMap::new();
         variables.insert(String::from("a"),5);
         variables.insert(String::from("b"),-7);
@@ -106,7 +106,7 @@ mod tests {
     }
 
     #[test]
-    fn can_not_override(){
+    fn should_not_be_able_to_redefine_numbers_and_operation_symbols(){
         let mut variables = VariableMap::new();
         variables.insert(String::from("+"),5);
         variables.insert(String::from("7"),5);
@@ -116,18 +116,18 @@ mod tests {
 
     }
     #[test]
-    fn multiplication(){
+    fn should_be_able_to_multiply(){
         assert_eq!(Postfix::new("7 8 *").evaluate(&Default::default()), Ok(56));
     }
 
     #[test]
-    fn division(){
+    fn should_be_able_to_divide(){
         assert_eq!(Postfix::new("60 5 /").evaluate(&Default::default()), Ok(12));
         assert_eq!(Postfix::new("12 5 /").evaluate(&Default::default()), Ok(2));
     }
 
     #[test]
-    fn number_expression() {
+    fn should_return_a_number_when_given_a_litteral() {
         assert_eq!(Postfix::new("7").evaluate(&Default::default()), Ok(7));
         assert_eq!(Postfix::new("8").evaluate(&Default::default()), Ok(8));
         assert_eq!(Postfix::new("-88").evaluate(&Default::default()), Ok(-88));
@@ -135,38 +135,39 @@ mod tests {
 
 
     #[test]
-    fn add(){
+    fn should_be_able_to_add_numbers(){
         assert_eq!(Postfix::new("7 8 +").evaluate(&Default::default()), Ok(15));
         assert_eq!(Postfix::new("7 7 +").evaluate(&Default::default()), Ok(14));
         assert_eq!(Postfix::new("1 3 + 4 +").evaluate(&Default::default()), Ok(8));
     }
 
     #[test]
-    fn sub(){
+    fn should_be_able_to_subtract_numbers(){
         assert_eq!(Postfix::new("7 8 -").evaluate(&Default::default()), Ok(-1));
         assert_eq!(Postfix::new("7 7 -").evaluate(&Default::default()), Ok(0));
         assert_eq!(Postfix::new("1 3 - 4 -").evaluate(&Default::default()), Ok(-6));
     }
 
     #[test]
-    fn mixed_operations(){
+    fn should_be_able_to_use_all_operations_together(){
         assert_eq!(Postfix::new("1 3 + 4 -").evaluate(&Default::default()), Ok(0));
         assert_eq!(Postfix::new("1 3 - 4 +").evaluate(&Default::default()), Ok(2));
+        assert_eq!(Postfix::new("5 6 * 1 3 - 4 + /").evaluate(&Default::default()), Ok(15));
     }
 
     #[test]
-    fn empty(){
+    fn empty_string_should_produce_error(){
         assert_eq!(Postfix::new(" ").evaluate(&Default::default()), Err(ParseError));
         assert_eq!(Postfix::new("").evaluate(&Default::default()), Err(ParseError));
     }
     #[test]
-    fn to_many_arguments(){
+    fn to_many_arguments_should_produce_error(){
         assert_eq!(Postfix::new("7 8").evaluate(&Default::default()), Err(ToManyArguments));
         assert_eq!(Postfix::new("7 8 9 +").evaluate(&Default::default()), Err(ToManyArguments));
     }
 
     #[test]
-    fn to_few_arguments(){
+    fn to_few_arguments_should_produce_error(){
         assert_eq!(Postfix::new("+").evaluate(&Default::default()), Err(ToFewArgements));
         assert_eq!(Postfix::new("9 -").evaluate(&Default::default()), Err(ToFewArgements));
         assert_eq!(Postfix::new("1 3 + +").evaluate(&Default::default()), Err(ToFewArgements));
